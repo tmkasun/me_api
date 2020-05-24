e7763a8999bde3f0ea0af03605d0311b6ea7dfa3
Kasun Thennakoon <tmkasun@gmail.com>
2020-05-24 22:15:59 +0530

    add json

--- generate_flask.sh
+++ generate_flask.sh
@@ -1,1 +1,1 @@
-curl -X POST -H "content-type:application/json" -d '{"swaggerUrl":"http://petstore.swagger.io/v2/swagger.json"}' https://generator.swagger.io/api/gen/servers/python-flask
+curl  -s -X POST -H "content-type:application/json" -d '{"swaggerUrl":"https://raw.githubusercontent.com/tmkasun/me_api/master/me-api.yaml"}' https://generator.swagger.io/api/gen/servers/python-flask | python3 -c "import sys, json; print(json.load(sys.stdin)['url'])"
